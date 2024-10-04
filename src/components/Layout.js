cc42c27359062adb91e0b372aa97af9601fcbd6d
Barrington <46790107+barrven@users.noreply.github.com>
2024-10-04 11:10:01 -0400

    Removed redundant onAuthStateChanged call.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,24 +2,22 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useEffect } from 'react';
 import { auth } from '@/firebase/config';
-import { onAuthStateChanged } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import Navbar from '@/components/Navbar';
 import { UserContext } from '@/services/UserContext'; // Import the context
 
 const Layout = ({ children }) => {
-  const [user] = useAuthState(auth);  // Destructure the user from the hook
+  const [user, loading] = useAuthState(auth);
   const router = useRouter();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (!user && router.pathname !== '/sign-in' && router.pathname !== '/sign-up') {
-        router.push('/sign-in');
-      }
-    });
 
-    return () => unsubscribe();
-  }, [router]);
+    if (!loading && !user && router.pathname !== '/sign-in' && router.pathname !== '/sign-up') {
+        console.log('----',user)
+        router.push('/sign-in');
+    }
+
+  }, [user, loading, router]);
 
   return (
     <UserContext.Provider value={user}>
